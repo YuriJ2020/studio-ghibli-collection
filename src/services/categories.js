053209064bcef1f4a73cd7ed74f3b4414baf2e9f
YuriJ <ytauchi@student.holmesglen.edu.au>
2021-04-06 21:47:38 +1000

    - More implementation added for assessment;

--- src/services/categories.js
+++ src/services/categories.js
@@ -1,11 +1,9 @@
 import _ from "lodash";
 
 class Category {
-  constructor(id, name, description, popularity) {
+  constructor(id, name) {
     this._id = id;
     this._name = name;
-    this._description = description;
-    this._popularity = popularity;
   }
   get id() {
     return this._id;
@@ -13,21 +11,39 @@ class Category {
   get name() {
     return this._name;
   }
+  describe() {
+    return `Category ID: ${this._id}; Category Name: ${this._name}`;
+  }
+}
+
+class MovieCategory extends Category {
+  constructor(id, name, description = '', popularity = 0) {
+    super(id, name);
+    this._description = description;
+    this._popularity = popularity;
+  }
   get description() {
     return this._description;
   }
   get popularity() {
     return this._popularity;
   }
+  describe() {
+    return `Movie Category Name: ${this._name}; Movie Category Popularity: ${this._popularity}`;
+  }
+  static comparePopularity(catA, catB) {
+    return catA.popularity - catB.popularity;
+  }
 };
 
 const categories = [
-  new Category("101", "Fantasy", "Fantasy Movie", 20),
-  new Category("102", "Family", "Family Movie", 30),
-  new Category("103", "War", "War Movie", 15),
-  new Category("104", "Romance", "Romance Movie", 25),
-  new Category("105", "Nature", "Nature Movie", 10),
+  new MovieCategory("101", "Fantasy", "Fantasy Movie", 25),
+  new MovieCategory("102", "Family", "Family Movie", 15),
+  new MovieCategory("103", "War", "War Movie", 10),
+  new MovieCategory("104", "Romance", "Romance Movie", 30),
+  new MovieCategory("105", "Nature", "Nature Movie", 20),
 ];
+categories.sort(MovieCategory.comparePopularity);
 
 export const getCategories = () => categories;
 export const getCategoryByID = (id) => _.find(categories, elem => elem.id === id);
