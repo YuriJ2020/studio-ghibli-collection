937b328a4b281c2a6b6c4fe77cf2c1365644d9bb
YuriJ <ytauchi@student.holmesglen.edu.au>
2021-04-06 20:48:12 +1000

    - Category class added;

--- src/services/categories.js
+++ src/services/categories.js
@@ -1,12 +1,33 @@
 import _ from "lodash";
 
+class Category {
+  constructor(id, name, description, popularity) {
+    this._id = id;
+    this._name = name;
+    this._description = description;
+    this._popularity = popularity;
+  }
+  get id() {
+    return this._id;
+  }
+  get name() {
+    return this._name;
+  }
+  get description() {
+    return this._description;
+  }
+  get popularity() {
+    return this._popularity;
+  }
+};
+
 const categories = [
-  { _id: "101", name: "Fantasy" },
-  { _id: "102", name: "Family" },
-  { _id: "103", name: "War" },
-  { _id: "104", name: "Romance" },
-  { _id: "105", name: "Nature" },
+  new Category("101", "Fantasy", "Fantasy Movie", 20),
+  new Category("102", "Family", "Family Movie", 30),
+  new Category("103", "War", "War Movie", 15),
+  new Category("104", "Romance", "Romance Movie", 25),
+  new Category("105", "Nature", "Nature Movie", 10),
 ];
 
 export const getCategories = () => categories;
-export const getCategoryByID = (id) => _.find(categories, { _id: id });
+export const getCategoryByID = (id) => _.find(categories, elem => elem.id === id);
